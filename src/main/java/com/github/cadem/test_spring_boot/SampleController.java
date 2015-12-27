deab24f5f59a64dff573a1c4ff431a1ccdeb5e37
Dominic (chendongming) <dongming.chen02@ele.me>
2015-12-28 01:27:16 +0800

    return a view

--- src/main/java/com/github/cadem/test_spring_boot/SampleController.java
+++ src/main/java/com/github/cadem/test_spring_boot/SampleController.java
@@ -1,22 +1,19 @@
 package com.github.cadem.test_spring_boot;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+import java.util.Date;
 
 @Controller
-@EnableAutoConfiguration
 public class SampleController {
 
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
-        System.out.println("run is finished at this time");
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
---
+++ src/main/java/com/github/cadem/test_spring_boot/SampleWebJspApplication.java
@@ -0,0 +1,15 @@
+package com.github.cadem.test_spring_boot;
+
+/**
+ * Created by chendongming on 12/27/15.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebJspApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleWebJspApplication.class, args);
+    }
+}
